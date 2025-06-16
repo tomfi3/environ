fa1219956462ef23c74e46982e59e08e86894c08
tomfi3 <146722878+tomfi3@users.noreply.github.com>
2025-06-16 17:54:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1270,5 +1270,5 @@ def set_chart_expanded(n, expanded):
     return not expanded
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))   # 10000 is a safe default
-    app.run(server="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
